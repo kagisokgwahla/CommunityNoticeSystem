5a2955226bf2c0afaee0ebff92c55ad254b061bb
lelethungalo@gmail.com <leletungalo@gmail.com>
2019-09-08 21:21:43 +0200

    profile fragments added

--- app/src/main/java/com/example/communitynoticesystem/settings.java
+++ app/src/main/java/com/example/communitynoticesystem/settings.java
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
 
 public class settings extends AppCompatActivity {
     private Toolbar mToolbar;
-    FragmentManager manager;
-    FragmentTransaction transaction;
+    private settingsStatePagerAdaper adaper;
+    private ViewPager mviewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +22,24 @@ public class settings extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Settings");
 
-        manager = getSupportFragmentManager();
-        transaction = manager.beginTransaction();
+        adaper = new settingsStatePagerAdaper(getSupportFragmentManager());
+        mviewPager = findViewById(R.id.container);
+        setUpViewPager(mviewPager);
+
+    }
+
+    public void setUpViewPager(ViewPager viewPager){
+        settingsStatePagerAdaper pagerAdaper = new settingsStatePagerAdaper(getSupportFragmentManager());
+        pagerAdaper.addFragment(new ProfileFragment(),"profile");
+        viewPager.setAdapter(pagerAdaper);
+    }
+
+    public void setMviewPager(int FragNumber){
+         mviewPager.setCurrentItem(FragNumber);
     }
 
     public void onUpdateProfile(View view){
-        ProfileFragment fragment = new ProfileFragment();
-        transaction.replace(R.id.proFeg,fragment);
-        transaction.commit();
+
     }
 
     public void checkReaminders(View view){
---
+++ app/src/main/java/com/example/communitynoticesystem/settingsStatePagerAdaper.java
@@ -0,0 +1,31 @@
+package com.example.communitynoticesystem;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+public class settingsStatePagerAdaper extends FragmentStatePagerAdapter {
+    private ArrayList<Fragment> mFragmentList = new ArrayList<>();
+    private ArrayList<String> mFragmentTitle = new ArrayList<>();
+
+    public settingsStatePagerAdaper(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment,String title){
+        mFragmentList.add(fragment);
+        mFragmentTitle.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
