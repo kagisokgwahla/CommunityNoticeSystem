97667faf6e3a83f8112d3aee742a5a65c3cadd30
lelethungalo@gmail.com <leletungalo@gmail.com>
2019-09-09 21:32:21 +0200

    profile fragments added

---
+++ app/src/main/java/com/example/communitynoticesystem/PostActivity.java
@@ -0,0 +1,22 @@
+package com.example.communitynoticesystem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+//import android.widget.Toolbar;
+
+public class PostActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+    }
+
+}
