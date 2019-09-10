fa1aeebc7bb6ab7258d05a6746f744df3f21e77c
kagiso <kgwkag001@myuct.ac.za>
2019-09-10 03:30:59 +0200

    Location available

--- app/src/main/java/com/example/communitynoticesystem/AdminActivity.java
+++ app/src/main/java/com/example/communitynoticesystem/AdminActivity.java
@@ -1,14 +1,45 @@
 package com.example.communitynoticesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminActivity extends AppCompatActivity {
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+    }
+
+    private void SendUserTologinActivity() {
+
+        Intent loginIntent = new Intent(AdminActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+    }
+
+    private void UserMenuSelector(MenuItem item){
+
+        switch(item.getItemId()){
+
+            case R.id.nav_logout_admin:
+                mAuth.signOut();
+                SendUserTologinActivity();
+                break;
+
+        }
     }
 }
